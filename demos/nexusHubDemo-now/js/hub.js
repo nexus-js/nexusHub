e3bea107b69098fa775a8af02f32501c6f504c4f
Jesse Allison <jtallison@lsu.edu>
2018-09-21 06:50:29 +0200

    Demo Version

---
+++ demos/nexusHubDemo-now/js/hub.js
@@ -0,0 +1,277 @@
+// hub.js
+
+'use strict';
+
+/** 
+	@class hub      
+	nexusHub core functions - used within node code
+	```js
+
+	hub.serverPort		// port for the server to run on. default = 3000
+	hub.receive() 
+	```
+	
+*/
+
+// Todo: create catch-all socket.on function that simply mirrors out any information received on the specified transmit channels.
+
+var Hub = function() {
+
+    this.serverPort = 3000;
+    this.ioClients = []; // list of clients who have logged in.
+    this.currentSection = 0; // current section.
+    this.sectionTitles = ["Welcome", "Preface", "Section 1", "Section 2", "Section 3", "End"];
+
+    // Specific clients who we only want one instance of
+
+    // Specific clients who we only want one instance of
+    this.discreteClients = {
+        display: {
+            id: null
+        },
+        controller: {
+            id: null
+        },
+        audio: {
+            id: null
+        },
+        max: {
+            id: null
+        }
+    }
+
+    this.display = {
+        id: ""
+    };
+    // conrollerID,
+    this.controller = {
+        id: ""
+    };
+    // audioID
+    this.audio = {
+        id: ""
+    };
+    // maxID
+    this.max = {
+        id: ""
+    };
+
+    this.channels = {};
+
+    this.io;
+}
+
+Hub.prototype.channel = function(oscMessage, nickname, sendTypeArray, callback) {
+    // console.log('channel nickname: ' + nickname);
+    if (!nickname) {
+        nickname = oscMessage;
+    }
+    // console.log('channel nickname: ' + nickname);
+
+    // console.log("socket? ", socket.id)
+
+    // console.log('channel sendTypeArray: ' + sendTypeArray);
+    if (!Array.isArray(sendTypeArray)) {
+        sendTypeArray = ['others'];
+    }
+    if (sendTypeArray.length == 0) {
+        sendTypeArray = ['others'];
+    }
+    // console.log('channel sendTypeArray: ' + sendTypeArray);
+    // FIXME: Don't use this yet.  It's not secure. Must be a better way.
+    if (!typeof callback === "function" || typeof callback === "undefined") {
+        //this.socket.on(oscMessage, callback);
+        // callback = function(data) {
+        //     hub.log(`tap: ${data}`);
+        //     hub.transmit(nickname, sendTypeArray, data);
+        // }
+
+        // console.log("Creating Callback");
+        let func = "hub.log(`" + nickname + ": ${data}`); hub.transmit('" + nickname + "', [" + sendTypeArray + "], data);";
+        console.log(func);
+        // callback = new Function("data", func);
+        // console.log("callback function? " + typeof callback);
+    }
+    // console.log("channel callback", callback);
+
+    this.channels[nickname] = { 'chan': oscMessage, 'sendTypes': sendTypeArray, 'callback': callback };
+
+};
+
+Hub.prototype.onConnection = function(sock) {
+    console.log("onConnection");
+    console.log("socket id: ", sock.id);
+    for (var key in this.channels) {
+        var oscMessage = this.channels[key].chan;
+        // console.log(oscMessage);
+        if (this.channels[key].hasOwnProperty('callback')) {
+            var callback = this.channels[key].callback
+                // console.log(key + " -> " + callback);
+            console.log("adding callback: ", key);
+            sock.on(oscMessage, callback);
+        }
+    }
+}
+
+// Transmit functions
+
+// others
+
+// all
+
+// self
+
+// display
+
+// controller
+
+// audio
+
+// max
+
+// group
+
+Hub.prototype.transmit = function(nickname, toWhom, data) {
+    let where;
+    if (toWhom instanceof String) {
+        where = [toWhom];
+    } else if (toWhom instanceof Array) {
+        where = toWhom;
+    } else if (this.channels.hasOwnProperty(nickname) && this.channels[nickname].sendTypes instanceof Array) {
+        where = this.channels[nickname].sendTypes;
+    } else {
+        where = ["others"];
+    }
+
+    let message;
+    if (this.channels.hasOwnProperty(nickname)) {
+        message = this.channels[nickname].chan;
+    } else {
+        message = nickname;
+    }
+
+
+    console.log("Channel: ", message, " Destinations: ", where);
+
+    // where.forEach(function(destination)  // had scope issues wanting to call this.log, etc. j
+    for (var i = 0; i < where.length; i++) {
+        let destination = where[i];
+        // this.log(`checking for ${destination}`);
+        if (destination == "self") {
+            socket.emit(message, data);
+        } else if (destination == "others") {
+            // sending to all clients except sender
+            // FIME: This sends to everyone! socket doesn't exist within this function. must find a way to use io to send to all but self
+            this.io.emit(message, data);
+        } else if (destination == "all") {
+            this.io.emit(message, data);
+        } else if (this.discreteClientCheck(destination)) {
+            // Individual Discrete Clients
+            this.io.to(this[destination].id).emit(message, data);
+        } else {
+            this.log(`destination ${destination} does not exist`);
+        }
+
+        // other possibilities?
+        // sending to all clients in 'game' room except sender
+        // if (destination == "group") socket.to('game').emit(data);
+        // sending to all clients in namespace 'myNamespace', including sender
+        // if (destination == "namespace") io.of('myNamespace').emit(data);
+        // sending to individual socketid (private message)
+        // if (destination == "user") socket.to( socketid ).emit(data);
+    };
+};
+
+Hub.prototype.discreteClientCheck = function(whom) {
+    if (this.discreteClients.hasOwnProperty(whom)) {
+        this.log(`discreteClientCheck for: ${whom} :: ${this.discreteClients[whom].id ? 1 : 0}`);
+        return this.discreteClients[whom].id ? 1 : 0;
+    } else {
+        return 0;
+    }
+}
+
+
+
+// **** SECTIONS ****
+
+// to set everyone's section to the same thing
+Hub.prototype.setSection = function(sect, toWhom) {
+    if (sect === 'undefined') {
+        sect = this.currentSection;
+    }
+
+    var title = this.getSection(sect);
+    this.io.sockets.emit('setSection', { sect: sect, title: title });
+
+    // hub.transmit('audio')
+};
+
+// TODO: Do I still need this?
+// hub.sendSection(currentSection);	 // Sets everyone's section
+Hub.prototype.sendSection = function(sect, toWhom) {
+    console.log("sendSection:", socket.id);
+    if (sect === 'undefined') {
+        sect = this.currentSection;
+    }
+
+    var title = this.getSection(sect);
+    // this.io.sockets.emit('setSection', { sect: sect, title: title });
+    this.transmit("setSection", toWhom, { sect: sect, title: title });
+
+};
+
+// Section shared from Max to UIs
+Hub.prototype.shareSection = function(sect) {
+    var title = this.getSection(sect);
+    this.io.sockets.emit('setSection', sect, title);
+};
+
+Hub.prototype.getSection = function(sect) {
+    var title = "none";
+
+    if (sect !== 'undefined') {
+        title = this.sectionTitles[sect];
+    } else {
+        title = this.sectionTitles[this.currentSection];
+    }
+
+    return title;
+};
+
+Hub.prototype.log = function(l) {
+    console.log('Hub Log: ' + l);
+};
+
+// TODO: instantiate SocketIO here instead of node file. 
+Hub.prototype.init = function(sio, publicFolder) {
+    // Setup web app - using express to serve pages
+    var express = require('../node_modules/express');
+    var http = require('http');
+    var serverPort = this.serverPort;
+    console.log("Port: " + this.serverPort);
+
+    var app = express();
+    app.use(express.static(publicFolder));
+    console.log(publicFolder);
+
+    // server is the node server (web app via express)
+    // this code can launch the server on port 80 and switch the user id away from sudo
+    // apparently this makes it more secure - if something goes awry it isn't running under the superuser.
+    var server = http.createServer(app)
+        .listen(serverPort, function(err) {
+            if (err) return cb(err);
+
+            var uid = parseInt(process.env.SUDO_UID); // Find out which user used sudo through the environment variable
+            if (uid) process.setuid(uid); // Set our server's uid to that user
+            console.log('Server\'s UID is now ' + process.getuid());
+        });
+
+    // start socket.io listening on the server
+    this.io = sio.listen(server);
+
+    console.log("Hub Helper Initialized!");
+}
+
+
+module.exports = Hub;
---
+++ demos/nexusHubDemo-now/nexusNode.js
@@ -0,0 +1,188 @@
+// ************************************************
+
+// NEXUS Hub Node Server
+//				Jesse Allison (2018)
+//
+//	To Launch:
+//		npm start
+//		- or -
+//		NODE_ENV=production sudo PORT=80 node nexusNode.js
+//		(sudo is required to launch on port 80.)
+
+// ************************************************
+
+var sio = require('socket.io');
+var publicFolder = __dirname + '/public';
+
+var NexusHub = require('./js/hub');
+var hub = new NexusHub();
+
+// update any server settings before initialization
+if (process.env.PORT) {
+    hub.serverPort = process.env.PORT;
+}
+
+hub.init(sio, publicFolder);
+
+
+
+// *********************
+// Set Hub Variables  or add more if you like.
+
+hub.currentSection = 0; // current section.
+hub.sectionTitles = ["Welcome", "Preface", "Section 1", "Section 2", "End"];
+
+// *********************
+
+
+
+// Respond to web sockets with socket.on
+hub.io.sockets.on('connection', function(socket) {
+    console.log("When am I called?");
+    var ioClientCounter = 0; // Can I move this outside into global vars?
+    this.socket = socket;
+    this.socketID = socket.id;
+
+    hub.channel('register', null, null, function(data) {
+        // TODO: iterate through data and add these properties dynamically.
+        // Can add any other pertinent details to the socket to be retrieved later
+
+        socket.username = typeof data.name !== 'undefined' ? data.name : "a_user";
+        socket.userColor = typeof data.color !== 'undefined' ? data.color : "#CCCCCC";
+        socket.userNote = typeof data.note !== 'undefined' ? data.note : " ";
+        socket.userLocation = typeof data.location !== 'undefined' ? data.location : { x: 0.5, y: 0.5 };
+
+        // **** Standard client setup ****
+        if (socket.username == "display") {
+            hub.display.id = socket.id;
+            hub.discreteClients.display.id = socket.id;
+            console.log("Hello display: " + hub.display.id);
+        }
+
+        if (socket.username == "controller") {
+            hub.controller.id = socket.id;
+            hub.discreteClients.controller.id = socket.id;
+            console.log("Hello Controller: " + hub.controller.id);
+        }
+
+        if (socket.username == "audioController") {
+            hub.audio.id = socket.id;
+            hub.discreteClients.audio.id = socket.id;
+            console.log("Hello Audio Controller: " + hub.audio.id);
+        }
+
+        if (socket.username == "maxController") {
+            hub.audio.id = socket.id;
+            hub.discreteClients.audio.id = socket.id;
+            console.log("Hello MaxMSP Controller: " + hub.max.id);
+        }
+
+        if (socket.username == "a_user") {
+            hub.ioClients.push(socket.id);
+        }
+
+        var title = hub.getSection(hub.currentSection);
+
+        //        socket.emit('chat', 'SERVER: You have connected. Hello: ' + username);
+
+        // hub.sendSection(hub.currentSection, ['self']);
+
+        // hub.io.sockets.emit('setSection', {sect: sect, title: title});
+        if (socket.username == "a_user") {
+            // oscClient.send('/causeway/registerUser', socket.id, socket.userColor, socket.userLocation[0],socket.userLocation[1], socket.userNote);
+            if (hub.audio.id) {
+                hub.io.to(hub.audio.id).emit('/causeway/registerUser', { id: socket.id, color: socket.userColor, locationX: socket.userLocation[0], locationY: socket.userLocation[1], note: socket.userNote }, 1);
+                // console.log("Added New User", {id: socket.id, color: socket.userColor, locationX: socket.userLocation[0], locationY: socket.userLocation[1], note: socket.userNote});
+            }
+        }
+    });
+
+    // Traditional socket assignments work just fine
+    socket.on('disconnect', function() {
+        // hub.ioClients.remove(socket.id);	// FIXME: Remove client if they leave
+        hub.log('SERVER: ' + socket.id + ' has left the building');
+    });
+
+    // **********************************************************
+    //******** Model for most nexusHub interactions create a channel and a response you want to have happen
+    // TODO: remove the need for the socket.broadcast.emit and uncomment the hub.transmit as the replacement.
+
+    hub.channel('test', 'test', ['others'], function(data) {
+        console.log('Adding in a new socket.on test with data:', data);
+        hub.log(`test ${data}`);
+        hub.transmit('test', null, data);
+    });
+
+    hub.channel('tap', null, ["others", "display"], function(data) {
+        hub.log(`tap ${data}`);
+        hub.transmit('tap', null, data);
+        //  socket.broadcast.emit('tap', data);  // just for others until a fix is made.
+    });
+
+    // Don't use auto callback creation yet, it's not secure.
+    // hub.channel('tap', null, ["others", "display", "audio"]);
+
+    // TODO: Should just demo this with tap ["others"] above.
+    hub.channel('tapOthers', null, ["others"], function(data) {
+        hub.log(`tapOthers ${data}`);
+        hub.transmit('tapOthers', null, data);
+        // socket.broadcast.emit('tapOthers', data);
+    });
+
+    hub.channel('shareToggle', null, ["others"], function(data) {
+        hub.log(`shareToggle ${data}`);
+        hub.transmit('shareToggle', null, data);
+    });
+
+    hub.channel('sharedSlider', null, null, function(data) {
+        // hub.transmit('sharedSlider', null, data);
+        socket.broadcast.emit('sharedSlider', data);
+    });
+
+    hub.channel('shareColor', null, ["others"], function(data) {
+        hub.log(`shareColor ${data}`);
+        hub.transmit('shareColor', null, data);
+    });
+
+    hub.channel('sendText', null, ["others", "display"], function(data) {
+        hub.log(`sendText ${data}`);
+        hub.transmit('sendText', null, data);
+    });
+
+    hub.channel('triggerPitch', null, ["others", "display"], function(data) {
+        hub.log(`triggerPitch ${data}`);
+        hub.transmit('triggerPitch', null, data);
+    });
+
+    hub.channel('triggerMaxPitch', null, ["max"], function(data) {
+        hub.log(`triggerMaxPitch ${data}`);
+        hub.transmit('triggerMaxPitch', null, data);
+    });
+
+    hub.channel('section', null, null, function(data) {
+        hub.log(`Section is now: ${data}`);
+        hub.currentSection = data;
+        hub.sendSection(hub.currentSection);
+    });
+
+    hub.channel('item', null, null, function(data) {
+        console.log(socket.id + " tapped item: " + data);
+        // This could be done with the sendTypes array, but if you want to overwrite what is being sent, here you go:
+        if (hub.discreteClientCheck('display')) {
+            hub.io.to(hub.discreteClients['display'].id).emit('itemback', { phrase: data, color: "socket.userColor" }, 1);
+        }
+        if (hub.discreteClientCheck('audio')) {
+            hub.io.to(hub.discreteClients[audio].id).emit('/item', { id: "socket.id", item: data }, 1);
+        }
+        hub.transmit('itemback', null, data);
+    });
+
+    hub.channel('end', null, ["others"], function(data) {
+        hub.log(`end ${data}`);
+        hub.transmit('end', null, data);
+    });
+
+    console.log("On Connect socket id: ", socket.id);
+    hub.onConnection(socket);
+
+});
